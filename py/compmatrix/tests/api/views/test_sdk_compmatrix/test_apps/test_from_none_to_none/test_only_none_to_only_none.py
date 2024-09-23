8f6977b11ca555d7b6f697276372eb214edf89da
Sean Francis N. Ballais <sean@seanballais.com>
2024-09-24 01:40:20 +0800

    Update test_only_none_to_only_none to include apps with no SDKs in the (none) to (none) cell.

--- py/compmatrix/tests/api/views/test_sdk_compmatrix/test_apps/test_from_none_to_none/test_only_none_to_only_none.py
+++ py/compmatrix/tests/api/views/test_sdk_compmatrix/test_apps/test_from_none_to_none/test_only_none_to_only_none.py
@@ -21,7 +21,7 @@ SDK_COMPMATRIX_APPS_ENDPOINT = f'{BASE_SDK_COMPMATRIX_ENDPOINT}/apps'
 
 @pytest.fixture(scope='module')
 def expected_apps(apps):
-    app_ids = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+    app_ids = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
     expected_apps = []
     for app_id in app_ids:
         encoded_obj = view_encoders.encode_app_model_object(apps[app_id])
@@ -91,10 +91,10 @@ def test_has_cursor_next_dir2(client, expected_apps, sdk_ids):
 
     expected_resp = {
         'data': {
-            'apps': apps[7:len(apps)],
+            'apps': apps[7:17],
             'total_count': len(apps),
             'start_cursor': query_utils.create_cursor_from_app_dict(apps[7]),
-            'end_cursor': query_utils.create_cursor_from_app_dict(apps[13])
+            'end_cursor': query_utils.create_cursor_from_app_dict(apps[16])
         }
     }
 
