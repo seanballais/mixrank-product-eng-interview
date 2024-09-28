4cd768cef4515e307a97205df77f15e47f8b5f48
Sean Francis N. Ballais <sean@seanballais.com>
2024-09-28 11:04:29 +0800

    Add arguments for the asset builder. Put watch mode behind a flag.

--- build-assets.py
+++ build-assets.py
@@ -1,4 +1,5 @@
-import os.path
+import argparse
+from argparse import ArgumentParser
 import subprocess
 from pathlib import Path
 import time
@@ -8,7 +9,10 @@ from watchdog.events import FileSystemEventHandler, FileSystemEvent
 from watchdog.observers.polling import PollingObserver
 
 
-def _build_css(main_scss_file: Path, dest_css_file: Path, build_dir: Path):
+def _build_css(main_scss_file: Path,
+               dest_css_file: Path,
+               build_dir: Path,
+               minify: bool):
     # Compile with SCSS.
     tmp_css_file: Path = build_dir / 'app.css'
     subprocess.run(['sass', str(main_scss_file), str(tmp_css_file)])
@@ -18,55 +22,61 @@ def _build_css(main_scss_file: Path, dest_css_file: Path, build_dir: Path):
         raw_css: str = f.read()
 
     with open(dest_css_file, 'w') as f:
-        f.write(rcssmin.cssmin(raw_css))
+        if minify:
+            f.write(rcssmin.cssmin(raw_css))
+        else:
+            f.write(raw_css)
 
 
-def _build_js(main_js_file: Path, dest_js_file: Path):
+def _build_js(main_js_file: Path, dest_js_file: Path, minify: bool):
     # --minify has been disabled to help with development, since esbuild
     # removes calls to console.log() when minify is on. Later on, we
     # can set minify on by default when we're building for production.
-    subprocess.run([
+    command: list[str] = [
         'esbuild',
         str(main_js_file),
         '--bundle',
-        # '--minify',
-        f'--outfile={str(dest_js_file)}'
-    ])
+    ]
+    if minify:
+        command.append('--minify')
+
+    command.append(f'--outfile={str(dest_js_file)}')
+
+    subprocess.run(command)
 
 
 class CSSHandler(FileSystemEventHandler):
     def __init__(self,
                  main_scss_file: Path,
                  dest_css_file: Path,
-                 build_dir: Path):
+                 build_dir: Path,
+                 minify: bool):
         self.main_scss_file = main_scss_file
         self.dest_css_file = dest_css_file
         self.build_dir = build_dir
+        self.minify = minify
 
     def on_any_event(self, event: FileSystemEvent):
         print(f':: [CSS] Changes detected in {event.src_path}. Rebuilding...')
-        _build_css(self.main_scss_file, self.dest_css_file, self.build_dir)
+        _build_css(self.main_scss_file,
+                   self.dest_css_file,
+                   self.build_dir,
+                   self.minify)
 
 
 class JSHandler(FileSystemEventHandler):
-    def __init__(self, main_js_file, dest_js_file):
+    def __init__(self, main_js_file: Path, dest_js_file: Path, minify: bool):
         self.main_js_file = main_js_file
         self.dest_js_file = dest_js_file
+        self.minify = minify
 
     def on_any_event(self, event: FileSystemEvent) -> None:
         print(f':: [JS] Changes detected in {event.src_path}. Rebuilding...')
 
-        _build_js(self.main_js_file, self.dest_js_file)
+        _build_js(self.main_js_file, self.dest_js_file, self.minify)
 
 
-def main():
-    # TODO: - Add an option to set the asset builder mode to development or
-    #         production. When it's set to development, minify will be
-    #         disabled. When it's set to production, it should be enabled.
-    #       - The default action of the asset builder is to just build the
-    #         assets and be done with it. We should implement that later on.
-    #         We should add a --watch option later too, which will make it
-    #         watch assets we're editing.
+def main(minify, watch):
     dest_dir: Path = Path('py/compmatrix/client/assets/dist')
 
     scss_dir: Path = Path('py/compmatrix/client/assets/raw/scss')
@@ -83,46 +93,70 @@ def main():
     print('🔨 Building assets...')
 
     print(':: Building CSS assets...')
-    _build_css(main_scss_file, dest_css_file, css_build_dir)
+    _build_css(main_scss_file, dest_css_file, css_build_dir, minify)
 
     print(':: Building JS assets...')
-    _build_js(main_js_file, dest_js_file)
+    _build_js(main_js_file, dest_js_file, minify)
 
     print('💖 Done!')
 
-    print(f'🔭 Watching SCSS directory: {str(scss_dir)}')
-    css_handler: CSSHandler = CSSHandler(main_scss_file,
-                                         dest_css_file,
-                                         css_build_dir)
+    if watch:
+        print(f'🔭 Watching SCSS directory: {str(scss_dir)}')
+        css_handler: CSSHandler = CSSHandler(main_scss_file,
+                                             dest_css_file,
+                                             css_build_dir,
+                                             minify)
 
-    print(f'🔭 Watching JS directory: {str(js_dir)}')
-    js_handler: JSHandler = JSHandler(main_js_file, dest_js_file)
+        print(f'🔭 Watching JS directory: {str(js_dir)}')
+        js_handler: JSHandler = JSHandler(main_js_file, dest_js_file, minify)
 
-    # We're using PollingObserver for now since we're coding in Windows, but
-    # running things inside WSL 2. File notification support between Windows
-    # and WSL is not yet added in the Windows 9P servers. For details, see:
-    #   https://youtu.be/lwhMThePdIo?si=NW7fBHIQN7iOosKR&t=3166
-    css_observer: PollingObserver = PollingObserver()
-    js_observer: PollingObserver = PollingObserver()
+        # We're using PollingObserver for now since we're coding in Windows,
+        # but running things inside WSL 2. File notification support between
+        # Windows and WSL is not yet added in the Windows 9P servers. For
+        # details, see:
+        #   https://youtu.be/lwhMThePdIo?si=NW7fBHIQN7iOosKR&t=3166
+        css_observer: PollingObserver = PollingObserver()
+        js_observer: PollingObserver = PollingObserver()
 
-    css_observer.schedule(css_handler, path=str(scss_dir), recursive=True)
-    js_observer.schedule(js_handler, path=str(js_dir), recursive=True)
+        css_observer.schedule(css_handler, path=str(scss_dir), recursive=True)
+        js_observer.schedule(js_handler, path=str(js_dir), recursive=True)
 
-    css_observer.start()
-    js_observer.start()
+        css_observer.start()
+        js_observer.start()
 
-    try:
-        while True:
-            time.sleep(1)
-    except KeyboardInterrupt:
-        print('🟥 Shutting down asset builder...')
-    finally:
-        css_observer.stop()
-        js_observer.stop()
+        try:
+            while True:
+                time.sleep(1)
+        except KeyboardInterrupt:
+            print('🟥 Shutting down asset builder...')
+        finally:
+            css_observer.stop()
+            js_observer.stop()
 
-        css_observer.join()
-        js_observer.join()
+            css_observer.join()
+            js_observer.join()
 
 
 if __name__ == '__main__':
-    main()
+    parser: ArgumentParser = ArgumentParser(prog='Asset Builder',
+                                            description='Builds assets.')
+    env_group = parser.add_mutually_exclusive_group(required=False)
+    env_group.add_argument('--dev', action='store_true')
+    env_group.add_argument('--prod', action='store_true')
+
+    parser.add_argument('--watch', action='store_true')
+    args: argparse.Namespace = parser.parse_args()
+
+    enable_minification: bool = False
+    environment: str = 'development' if args.dev else 'production'
+    if args.watch and args.prod:
+        environment = 'development'
+        print('⚠️ Watch mode is enabled. Changing environment to development. '
+              'Watch mode is only allowed during development.')
+    elif args.prod:
+        enable_minification = True
+
+    print(f'⚙️ Environment: {environment.capitalize()}')
+    print(f'⚙️ Minification Enabled: {str(enable_minification)}')
+
+    main(enable_minification, args.watch)
